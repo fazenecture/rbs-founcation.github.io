492bcbb23bee5ecc8f106a4c230dc7c482079121
Vivek <37245253+fazenecture@users.noreply.github.com>
2021-06-02 05:39:46 +0530

    yele

--- rbs/lib/home.dart
+++ rbs/lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rbs/Submitreport.dart';
-
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   static String id = 'home_screen';
@@ -17,137 +17,165 @@ class _HomeScreenState extends State<HomeScreen> {
       color: Color(0xFF171717),
       child: Column(
         children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Image.network(
-                'https://images.unsplash.com/photo-1573288881762-6ec87577a176?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                width: double.infinity,
-              ),
-              Image.network(
-                'https://images.unsplash.com/photo-1573288881762-6ec87577a176?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                width: double.infinity,
-                color: Colors.black.withOpacity(0.5),
-              ),
-              Container(
-                alignment: AlignmentDirectional.center,
-                margin: EdgeInsets.fromLTRB(0, 90, 0, 0),
-                child: Text(
-                  'RBS\nFOUNDATION',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(decoration: TextDecoration.none,
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 60, 20, 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello!',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontFamily: 'Lato',
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'suppp!bitch',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontFamily: 'Lato',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  CupertinoIcons.profile_circled,
                   color: Colors.white,
-                  fontSize: 30,
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'Lato',
-                  ),
+                  size: 36,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 70, 0, 30),
+            child: Lottie.network(
+              'https://assets4.lottiefiles.com/packages/lf20_r71cen62.json',
+              width: 340,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 40, 10, 20),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13)),
+                        color: Color(0xFF242424),
+                        child: InkWell(
+                          splashColor: Color(0xFF383838),
+                          borderRadius: BorderRadius.circular(13),
+                          onTap: () {
+                            Navigator.pushNamed(context, subRep.id);
+                          },
+                          child: Stack(
+                            children: [
+                              SizedBox(
+                                height: 200,
+                              ),
+                              Center(
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      child: Image(
+                                        image: AssetImage('images/document.png'),
+                                        height: 80,
+                                        width: 80,
+                                      ),
+                                      margin: EdgeInsets.fromLTRB(0, 45, 0, 20),
+                                    ),
+                                    Container(
+                                      alignment: AlignmentDirectional.center,
+                                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                      child: Text(
+                                        'Submit a Report',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                          fontFamily: 'Lato',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
-          Stack(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 40, 10, 20),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(13)
-                  ),
-                  color: Color(0xFF474747),
+              Expanded(
+                child: Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 40, 10, 20),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13)),
+                        color: Color(0xFF242424),
+                        child: InkWell(
+                          splashColor: Color(0xFF383838),
+                          borderRadius: BorderRadius.circular(13),
+                          onTap: () {
 
-                  child: InkWell(
-                    splashColor: Color(0xFF383838),
-                    borderRadius: BorderRadius.circular(13),
-                    onTap: (){
-                      Navigator.pushNamed(context, subRep.id);
-                    },
-                    child: Stack(
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: 160,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              child: Image(
-                                image: AssetImage('images/file.png'),
-                                height: 50,
-                                width: 50,
+                          },
+                          child: Stack(
+                            children: [
+                              SizedBox(
+                                height: 200,
                               ),
-                              margin: EdgeInsets.fromLTRB(30, 50, 0, 0),
-                            ),
-                            Container(
-                              alignment: AlignmentDirectional.center,
-                              margin: EdgeInsets.fromLTRB(30, 60, 0, 0),
-                              child: Text(
-                                'Submit a Report',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                  fontFamily: 'Lato',
+                              Center(
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      child: Image(
+                                        image: AssetImage('images/donation (1).png'),
+                                        height: 80,
+                                        width: 80,
+                                      ),
+                                      margin: EdgeInsets.fromLTRB(0, 45, 0, 20),
+                                    ),
+                                    Container(
+                                      alignment: AlignmentDirectional.center,
+                                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                      child: Text(
+                                        'Donate',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                          fontFamily: 'Lato',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
-          Stack(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(13)
-                  ),
-                  color: Color(0xFF474747),
-
-                  child: InkWell(
-                    splashColor: Color(0xFF383838),
-                    borderRadius: BorderRadius.circular(13),
-                    onTap: (){
-                      // Navigator.pushNamed(context, subRep.id);
-                    },
-                    child: Stack(
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: 160,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              child: Image(
-                                image: AssetImage('images/donation.png'),
-                                height: 50,
-                                width: 50,
-                              ),
-                              margin: EdgeInsets.fromLTRB(30, 50, 0, 0),
-                            ),
-                            Container(
-                              alignment: AlignmentDirectional.center,
-                              margin: EdgeInsets.fromLTRB(30, 60, 0, 0),
-                              child: Text(
-                                'Donate',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                  fontFamily: 'Lato',
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
+              ),
             ],
           ),
         ],
